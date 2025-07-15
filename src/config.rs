b80abb46f567fa13879d2afb112512c38a12b543
fubuki4649 <boku@kaneki.dev>
2025-07-15 02:59:34 -0400

    Rewrote sunrise watcher

--- src/config.rs
+++ src/config.rs
@@ -6,3 +6,7 @@ pub type SelectedLocationProvider = IpLocationProvider;
 /// The period of time for which the location data is valid for (in seconds)
 // 3600 seconds = 1 hour
 pub const LOCATION_TTL: u64 = 3600;
+
+/// How often to check for sunset
+// 600 seconds = 10 minutes
+pub const SUNSET_CHECK_FREQUENCY: u64 = 600;
--- src/dbus_portal/wrapper.rs
+++ src/dbus_portal/wrapper.rs
@@ -4,12 +4,12 @@ use log::info;
 use zbus::blocking::{connection, Connection};
 use zbus::zvariant::Value::U32;
 
-struct PortalInterface {
+pub struct PortalWrapper {
     conn: Connection,
 }
 
-impl PortalInterface {
-    fn new() -> Result<Self, Error> {
+impl PortalWrapper {
+    pub fn new() -> Result<Self, Error> {
         // Initialize a DBus connection
         let conn = connection::Builder::session()?
                 .name("org.freedesktop.impl.dbus_portal.desktop.asahi")?
@@ -24,7 +24,12 @@ impl PortalInterface {
         Ok(Self { conn })
     }
 
-    fn set_darkmode(&self, value: u32) {
+    /// 0 - No Preference
+    ///
+    /// 1 - Dark Mode
+    ///
+    /// 2 - Light Mode
+    pub fn set_darkmode(&self, value: u32) {
         let iref = self.conn.object_server()
             .interface::<_, Portal>("/org/freedesktop/dbus_portal/desktop")
             .expect("Interface not found at path");
